836e35caa358585241b2a5ad9735ba22a0706777
Romain Prignon <pro.rprignon@gmail.com>
2017-03-26 20:10:11 +0200

    feat: add eslint rules

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    "eol-last": ["error", "always"],
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "max-len": ["error", 80],
+    "no-trailing-spaces": "error"
+  }
+}
